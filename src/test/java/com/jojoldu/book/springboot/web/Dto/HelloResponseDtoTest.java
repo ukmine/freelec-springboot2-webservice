9fc6f2106194bda0a3e0d07e9c494baed72152c1
ukminechoi <bach5yun@gmail.com>
2022-11-01 14:10:17 +0900

    start

---
+++ src/test/java/com/jojoldu/book/springboot/web/Dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.springboot.web.Dto;
+
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트(){
+        //given
+        String name = "귀요미";
+        int amount = 1000000000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
